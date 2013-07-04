12528bedf8175189369596e55235495cd2ffda50
Amir Rajan <ar@amirrajan.net>
2013-07-04 15:17:27 -0500

    initial commit

---
+++ DNUG.UITests/Program.fs
@@ -0,0 +1,22 @@
+﻿open canopy
+
+open runner
+
+start firefox
+
+let reset _ =
+    let seed = new Oak.Controllers.SeedController()
+    seed.PurgeDb() |> ignore
+    seed.All() |> ignore
+    () 
+
+before(reset)
+
+test(fun _ ->
+    describe "hello world"
+    url "http://localhost:3000/"
+)
+
+run()
+
+quit()
---
+++ DNUG/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Oak;
+using Massive;
+
+namespace DNUG.Controllers
+{
+    public class HomeController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
