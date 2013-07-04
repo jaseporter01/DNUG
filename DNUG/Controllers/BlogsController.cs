93bbc3a0a2694a322bd47353a7b3808452e2eeb7
Amir Rajan <ar@amirrajan.net>
2013-07-04 15:23:30 -0500

    added blogs controller, retrieving blogs from repo, but haven't set up the database table yet

---
+++ DNUG/Controllers/BlogsController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Oak;
+using Massive;
+
+namespace DNUG.Controllers
+{
+    public class Blogs : DynamicRepository { }
+
+    public class BlogsController : BaseController
+    {
+        Blogs blogs = new Blogs();
+
+        public ActionResult List()
+        {
+            return Json(new
+            {
+                Blogs = blogs.All(),
+                CreateUrl = Url.RouteUrl(new { action = "Create" })
+            });
+        }
+    }
+}
